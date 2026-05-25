41463f61dab0b13f1d6f0934f14b012b6ff0aaf1
maximomalo <maxmalouf03@gmail.com>
2026-05-24 22:30:52 -0400

    added deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
-import path from "path";
-
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio-page/',
   plugins: [react(), tailwindcss()],
-  resolve:  {
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     }
